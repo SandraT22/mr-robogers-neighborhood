c4187a78590e155d4f68232dd7fb4b62f8c45f16
Sandra Tena <sandratena06@gmail.com>
2022-06-10 11:21:51 -0700

    Complete test 5: replace 1 with boop and update readme to keep track of completed tests

--- js/scripts.js
+++ js/scripts.js
@@ -23,23 +23,43 @@
 //   return digitsArray;
 // };
 // This test will identify the number 2 within the array.
-function beepBoop(number) {
-  const digits = parseInt(number);
-  let digitsArray = [];
-  let oneFlag = false;
-  let twoFlag = false;
-  for (let i = 0; i <= digits; i += 1) {
-    digitsArray.push(i);
-    if (digitsArray[i] === 1) {
-        oneFlag = true;
-    } else if (digitsArray[i] === 2) {
-      twoFlag = true;
-    };
-  } 
-  console.log(twoFlag);
-  return digitsArray;
-};
-
+// function beepBoop(number) {
+//   const digits = parseInt(number);
+//   let digitsArray = [];
+//   let oneFlag = false;
+//   let twoFlag = false;
+//   for (let i = 0; i <= digits; i += 1) {
+//     digitsArray.push(i);
+//     if (digitsArray[i] === 1) {
+//         oneFlag = true;
+//     } else if (digitsArray[i] === 2) {
+//       twoFlag = true;
+//     };
+//   } 
+//   console.log(twoFlag);
+//   return digitsArray;
+// };
+// This test will identify the number 3 within the array.
+// function beepBoop(number) {
+//   const digits = parseInt(number);
+//   let digitsArray = [];
+//   let oneFlag = false;
+//   let twoFlag = false;
+//   let threeFlag = false;
+//   for (let i = 0; i <= digits; i += 1) {
+//     digitsArray.push(i);
+//     if (digitsArray[i] === 1) {
+//         oneFlag = true;
+//     } else if (digitsArray[i] === 2) {
+//       twoFlag = true;
+//     } else if (digitsArray[i] === 3) {
+//       threeFlag = true;
+//     };
+//   } 
+//   console.log(threeFlag);
+//   return digitsArray;
+// };
+// This test will replace a number containing the number 1 with the word "Boop!".
 function beepBoop(number) {
   const digits = parseInt(number);
   let digitsArray = [];
@@ -49,13 +69,13 @@ function beepBoop(number) {
   for (let i = 0; i <= digits; i += 1) {
     digitsArray.push(i);
     if (digitsArray[i] === 1) {
-        oneFlag = true;
-    } else if (digitsArray[i] === 2) {
-      twoFlag = true;
-    } else if (digitsArray[i] === 3) {
-      threeFlag = true;
-    };
+        digitsArray[1] = "Boop";
+    } 
+  //   else if (digitsArray[i] === 2) {
+  //     twoFlag = true;
+  //   } else if (digitsArray[i] === 3) {
+  //     threeFlag = true;
+  //   };
   } 
-  console.log(threeFlag);
   return digitsArray;
 };
