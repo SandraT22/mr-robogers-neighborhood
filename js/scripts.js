ca05deeace51ea54260fa3de354351bc6e02fcfd
Sandra Tena <sandratena06@gmail.com>
2022-06-10 10:49:16 -0700

    Complete test 2 - identify 1 within the array

--- js/scripts.js
+++ js/scripts.js
@@ -7,3 +7,16 @@ function beepBoop(number) {
   }
   return digitsArray;
 };
+// This test will identify the number 1 within the array.
+function beepBoop(number, index) {
+  const digits = parseInt(number);
+  let digitsArray = [];
+  let oneFlag = false;
+  for (let i = 0; i <= digits; i += 1) {
+    digitsArray.push(i);
+  } if (digitsArray[index] === "1") {
+      oneFlag = true;
+  };
+  console.log("looped");
+  return digitsArray;
+};
